db339a98d5bc12b08a9584475b77c701cd305f70
Mark Patka <mark.kaz.1998@gmail.com>
2024-08-25 21:44:42 +0300

    Adds DI Into The Startup Project

--- Presentation/PrintManager.Api/Program.cs
+++ Presentation/PrintManager.Api/Program.cs
@@ -1,10 +1,16 @@
+using PrintManager.Application;
+using PrintManager.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
 }
 
 var app = builder.Build();
 {
+    
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
