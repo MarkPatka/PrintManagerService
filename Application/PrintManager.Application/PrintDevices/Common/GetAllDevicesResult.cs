af11ad12c128fd235a353af39ffdebb2391ced84
usr <usr@PC1>
2024-08-26 17:31:03 +0300

    Scaffolds Default CQRS Structure

---
+++ Application/PrintManager.Application/PrintDevices/Common/GetAllDevicesResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintManager.Application.PrintDevices.Common
+{
+    internal class GetAllDevicesResult
+    {
+    }
+}
---
+++ Application/PrintManager.Application/PrintDevices/Queries/GetAllDevicesQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using ErrorOr;
+using PrintManager.Application.PrintDevices.Common;
+
+namespace PrintManager.Application.Departments.Queries;
+
+public record GetAllDevicesQuery() : IRequest<ErrorOr<List<GetAllDevicesResult>>>;
