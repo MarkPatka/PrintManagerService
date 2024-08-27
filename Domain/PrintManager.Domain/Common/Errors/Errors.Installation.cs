f39f37fc25ef80ba2525cf5445d5f3078f703bcc
usr <usr@PC1>
2024-08-27 13:07:51 +0300

    Adds Custom Global Errors Handling

---
+++ Domain/PrintManager.Domain/Common/Errors/Errors.Installation.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+
+namespace PrintManager.Domain.Common.Errors;
+
+public static partial class Errors
+{
+    public static class Installation
+    {
+        public static Error ValidationError => Error.Validation(
+            code: "The installation data is invalid");
+
+        public static Error Error => Error.Validation(
+            code: "The request body is Empty");
+
+        public static Error ServerError => Error.Failure(
+            code: "Internal server error");
+
+        public static Error DefaultDeviceDelete => Error.Conflict(
+            code: "Unable to delete an installation of default device");
+    }
+}
